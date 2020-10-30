19290a629591afe04f5ca112ea2ff96477df6bd2
njtak <jeongtak@sp1soft.com>
2020-10-30 10:18:01 +0900

    merge 예정

---
+++ src/thisisjava/normal/NormalTest.java
@@ -0,0 +1,16 @@
+package thisisjava.normal;
+
+public class NormalTest {
+
+	public static void main(String[] args) {
+		int num = 65;
+		char charecter = 'A';
+		
+		
+		System.out.println("charecter = " + (int)charecter);
+		System.out.println("charecter = " + (char)num);
+		System.out.println("charecter = " + num);
+		System.out.printf("%c=%d(#x) %n", charecter, num, num);
+	}
+}
+
